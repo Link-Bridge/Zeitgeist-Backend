a581512610676f9764eae62eb34fc21ffa8b1311
Daniel Hurtado <a01707774@tec.mx>
2024-03-24 23:16:13 -0500

    feat(db): setup prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { PrismaClient } from '@prisma/client';
 import express, { Express } from 'express';
 import swaggerJSDoc, { OAS3Options } from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import baseRouter from './api/routes/index.routes'; // Asegúrate de que la ruta sea correcta
-import { swaggerOptions } from './config/swagger-api.config'; // Asegúrate de que la ruta sea correcta
-import { EnvConfigKeys } from './utils/constants'; // Asegúrate de que la ruta sea correcta
+import baseRouter from './api/routes/index.routes';
+import { swaggerOptions } from './config/swagger-api.config';
+import { EnvConfigKeys } from './utils/constants';
 
+export const Prisma = new PrismaClient();
 const app: Express = express();
 
 const HOST: string = process.env[EnvConfigKeys.HOST] || 'localhost';
@@ -12,10 +14,8 @@ const PORT: number = process.env[EnvConfigKeys.PORT] ? parseInt(process.env[EnvC
 
 app.use(express.json());
 
-// Corrige la ruta para incluir el slash inicial
 app.use(baseRouter);
 
-// Genera la especificación de Swagger utilizando las opciones configuradas
 const swaggerSpec = swaggerJSDoc(swaggerOptions as OAS3Options);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
