5b3e87377c8c204ff93de1afa4066abfe277710f
Carlos Salguero <carlossalguero441@gmail.com>
2024-04-07 14:19:32 -0600

    fix(dummyRoutes): added removed comment

--- src/api/routes/dummy.routes.ts
+++ src/api/routes/dummy.routes.ts
@@ -3,6 +3,33 @@ import { getDummyData } from '../controllers/dummy.controller';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /dummy/data:
+ *   get:
+ *     summary: Obtiene datos dummy
+ *     tags:
+ *       - Dummy
+ *     description: Retorna una lista de datos dummy.
+ *     responses:
+ *       200:
+ *         description: Una lista de datos dummy
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: El ID del dato dummy
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     description: El nombre del dato dummy
+ *                     example: "Dato Dummy"
+ */
 router.get('/data', getDummyData);
 
 export { router as DummyRouter };
