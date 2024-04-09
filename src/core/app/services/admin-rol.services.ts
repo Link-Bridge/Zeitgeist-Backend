a7d1d68958db72563c8cb7c7d53a0d6c33c6d060
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2024-04-09 17:12:26 -0600

    feat(services): map RoleRepository.findById to updateUserRol service

--- src/core/app/services/admin-rol.services.ts
+++ src/core/app/services/admin-rol.services.ts
@@ -1,15 +1,17 @@
 import { EmployeeEntity } from '../../domain/entities/employee.entity';
 import { RoleEntity } from '../../domain/entities/role.entity';
-import {EmployeeRepository} from "../../infra/repositories/employee.repository";
+import { EmployeeRepository } from "../../infra/repositories/employee.repository";
+import { RoleRepository } from "../../infra/repositories/role.repository";
 
 async function updateUserRol(userId: string, roleId: string): Promise<void> {
     try {
         // Check if user exists
-        const data: EmployeeEntity = await EmployeeRepository.findById(userId);
-        
+        const employee: EmployeeEntity = await EmployeeRepository.findById(userId);
+
         // Check if role exists
-        
-        
+        const role: RoleEntity = await RoleRepository.findById(roleId);
+
+
         // Update User role
 
     } catch (error: any) {
