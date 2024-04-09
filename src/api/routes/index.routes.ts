811c274918ccf321a20ba51c5b6511c14f74d160
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2024-04-09 15:56:37 -0600

    feat(routes): add admin.routes

--- src/api/routes/index.routes.ts
+++ src/api/routes/index.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { DummyRouter } from './dummy.routes';
+import { AdminRouter } from './admin.routes';
 
 const baseRouter = Router();
 
+const V1_PATH = '/api/v1';
+
 baseRouter.use('/dummy', DummyRouter);
+baseRouter.use(`${V1_PATH}/admin`, AdminRouter);
 
 export { baseRouter };
