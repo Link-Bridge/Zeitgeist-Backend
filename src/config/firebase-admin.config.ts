40cb6465de86459c55656590c0a20ee5056a9bf7
Daniel Hurtado <a01707774@tec.mx>
2024-04-10 22:41:38 -0600

    fix(firebase): use firebase-admin

--- src/config/firebase-admin.config.ts
+++ src/config/firebase-admin.config.ts
@@ -1,18 +1,13 @@
 import * as admin from 'firebase-admin';
 import { EnvConfigKeys } from '../utils/constants';
 
-const firebaseConfig = {
-  apiKey: process.env[EnvConfigKeys.FIREBASE_API_KEY],
-  authDomain: process.env[EnvConfigKeys.FIREBASE_AUTH_DOMAIN],
-  projectId: process.env[EnvConfigKeys.FIREBASE_PROJECT_ID],
-  storageBucket: process.env[EnvConfigKeys.FIREBASE_STORAGE_BUCKET],
-  messagingSenderId: process.env[EnvConfigKeys.FIREBASE_MESSAGING_SENDER_ID],
-  appId: process.env[EnvConfigKeys.FIREBASE_APP_ID],
-};
+const privateKey = process.env[EnvConfigKeys.FIREBASE_PRIVATE_KEY];
+if (!privateKey) {
+  throw new Error('INTERNAL_SERVER_ERROR: Firebase private key is not set');
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-  databaseURL: 'https://linkbridge-internal-test.firebaseio.com', // TODO: check this
+  credential: admin.credential.cert(JSON.parse(privateKey) as admin.ServiceAccount),
 });
 
 export default admin;
