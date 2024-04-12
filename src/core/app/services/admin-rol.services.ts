774e0a14f48b0fcf21a445e67e16adfe31acd61f
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2024-04-12 16:42:57 -0600

    fix(services): delete useless code

--- src/core/app/services/admin-rol.services.ts
+++ src/core/app/services/admin-rol.services.ts
@@ -1,14 +1,7 @@
 import { EmployeeRepository } from "../../infra/repositories/employee.repository";
-import { RoleRepository } from "../../infra/repositories/role.repository";
 
 async function updateUserRol(userId: string, roleId: string): Promise<void> {
     try {
-        // Check if user exists
-        await EmployeeRepository.findById(userId);
-
-        // Check if role exists
-        await RoleRepository.findById(roleId);
-
         // Update User role
         await EmployeeRepository.updateRoleById(userId, roleId);
 
