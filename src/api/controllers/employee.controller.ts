18c82fc3c1839c244cac70970bdb4f6aa9a9c6f0
Frida Bailleres <a01708633@tec.mx>
2024-04-13 15:36:26 -0600

    docs(auth): comments a repository

--- src/api/controllers/employee.controller.ts
+++ src/api/controllers/employee.controller.ts
@@ -16,3 +16,37 @@ async function createUser(req: Request, res: Response) {
 }
 
 export const EmployeeController = { createUser };
+
+
+
+// const createUser = async (req: Request, res: Response) => {
+//   try {
+//     // Verifica si los detalles del empleado del middleware de autenticación están presentes y son válidos
+//     if (!req.employee || !req.employee.email) {
+//       return res.status(401).json({ message: 'La información de autorización está incompleta o falta.' });
+//     }
+
+//     // Recopila datos adicionales del cuerpo de la solicitud si es necesario
+//     const employeeData = {
+//       ...req.body,
+//       email: req.employee.email,  // Asegura usar el email del token verificado
+//       name: req.employee.name || req.body.name,  // Opcional: Prefiere el nombre del token, alternativa del cuerpo de la solicitud
+//       user_id: req.employee.uid,  // Incluye el ID de usuario único de la autenticación de Firebase
+//     };
+
+//     // Crea el empleado usando el servicio
+//     const employee = await EmployeeService.create(employeeData);
+
+//     // Devuelve los datos del empleado creado con un código de estado 201
+//     res.status(201).json({ data: employee });
+//   } catch (error) {
+//     // Maneja diferentes tipos de errores adecuadamente
+//     if (error instanceof ValidationError) {
+//       return res.status(400).json({ message: error.message });
+//     }
+//     // Respaldo de error genérico
+//     res.status(500).json({ message: 'Error interno del servidor: ' + error.message });
+//   }
+// };
+
+// export const EmployeeController = { createUser };
