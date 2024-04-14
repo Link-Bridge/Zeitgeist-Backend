9798e149ce4ca82717150fa6f035fa2092a431ba
Daniel Hurtado <a01707774@tec.mx>
2024-04-14 13:28:09 -0600

    feat(controller): validate inputs in controller

--- src/api/controllers/employee.controller.ts
+++ src/api/controllers/employee.controller.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
 import { EmployeeService } from '../../core/app/services/employee.service';
+
+const createEmployeeSchema = z.object({
+  firstName: z.string().min(1, { message: 'First name cannot be empty' }),
+  lastName: z.string().min(1, { message: 'Last name cannot be empty' }),
+  email: z.string().email({ message: 'Invalid email format' }),
+  imageUrl: z.string().url().optional(),
+});
 
 /**
  * Creates a new employee
- * @param req 
- * @param res 
+ * @param req
+ * @param res
  * @returns Employee entity
  */
 async function createUser(req: Request, res: Response) {
   try {
-    if (!req.body || !req.body.email) {
-      return res.status(401).json({ message: 'Invalid User input' });
+    const parsedBody = createEmployeeSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json({ message: 'Validation error', errros: parsedBody.error.issues });
     }
 
     const employee = await EmployeeService.create({
