7c07708e81f4b3a88080a536bd9ba41e364d4704
A01709150 <A01709150@tec.mx>
2024-04-13 23:31:11 -0600

    fix: Changed route name to english

--- src/api/routes/admin.routes.ts
+++ src/api/routes/admin.routes.ts
@@ -3,6 +3,6 @@ import { updateUserRol } from "../controllers/admin.controller";
 
 const router = Router();
 
-router.put("/rol", updateUserRol);
+router.put("/role", updateUserRol);
 
 export { router as AdminRouter };
