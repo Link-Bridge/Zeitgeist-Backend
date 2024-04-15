6597ce4ab4b2e76109fcfdac07d6ca04c33181f9
salgue441 <carlossalguero441@gmail.com>
2024-04-15 16:34:13 -0600

    feat(indexRoutes): added new router from tasks

--- src/api/routes/index.routes.ts
+++ src/api/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { DummyRouter } from './dummy.routes';
 import { ProjectRouter } from './project.routes';
+import { TaskRouter } from './task.routes';
 
 const baseRouter = Router();
 
@@ -8,5 +9,6 @@ const V1_PATH = '/api/v1';
 
 baseRouter.use('/dummy', DummyRouter);
 baseRouter.use(`${V1_PATH}/project`, ProjectRouter);
+baseRouter.use(`${V1_PATH}/tasks`, TaskRouter);
 
 export { baseRouter };
