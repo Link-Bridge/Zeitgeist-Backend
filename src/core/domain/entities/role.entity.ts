8eed5c9823d85021e2b35b3794ba41b17eb9df3d
A01709150 <A01709150@tec.mx>
2024-04-15 19:08:32 -0600

    fix(backend): role entity documentation

--- src/core/domain/entities/role.entity.ts
+++ src/core/domain/entities/role.entity.ts
@@ -1,6 +1,35 @@
+/**
+ * @brief The class establishes the structure from the entity Role
+ *
+ * @param id: string
+ * @param title: string
+ * @param createdAt: Date
+ * @param updatedAt: Date
+ *
+ * @return void
+ *
+ * @description The structure contains the data from the scheme Role
+ * 
+ */
+
 export interface RoleEntity {
+    /**
+     * @param id: string: The id of the role.
+     */
     id: string;
+
+    /**
+     * @param title string: The title of the role.
+     */
     title: string;
+
+    /**
+     * @param createdAt: Date: The date when the role was created.
+     */
     createdAt: Date;
+
+    /**
+     * @param updatedAt Date: The date when the role was updated.
+     */
     updateAt?: Date;
 }
