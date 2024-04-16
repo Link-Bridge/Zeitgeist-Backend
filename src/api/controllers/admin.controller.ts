a48563960c4b679de7b6b2be16cf6c0007366e5f
A01709150 <A01709150@tec.mx>
2024-04-16 17:42:50 -0600

    Fix(backend): added Zod in admin.controller

--- src/api/controllers/admin.controller.ts
+++ src/api/controllers/admin.controller.ts
@@ -1,32 +1,25 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
 import { updateUserRole as updateUserRoleService } from '../../core/app/services/admin-role.services';
 
-interface UpdateUserRoleBody {
-  userId?: string;
-  roleId?: string;
-}
+const updateUserRoleSchema = z.object({
+  userId: z.string().min(1, { message: 'User ID cannot be empty' }),
+  roleId: z.string().min(1, { message: 'Role ID cannot be empty' }),
+});
 
 async function updateUserRole(req: Request, res: Response) {
   try {
-    const { userId, roleId }: UpdateUserRoleBody = req.body;
-
-    if (userId === undefined) {
-      throw new Error('User id is undefined');
-    }
-    if (roleId === undefined) {
-      throw new Error('Role id is undefined');
-    }
-    if (userId.trim() === '') {
-      throw new Error('User id is undefined');
-    }
-    if (roleId.trim() === '') {
-      throw new Error('Role id is undefined');
-    }
+    // Validating request body against the schema
+    const { userId, roleId } = updateUserRoleSchema.parse(req.body);
 
     // Connect with service
     const employee = await updateUserRoleService(userId, roleId);
     res.status(200).json({ data: employee });
   } catch (error: any) {
+    // Handling Zod errors or other types of errors
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ errors: error.errors });
+    }
     res.status(500).json({ message: error.message });
   }
 }
