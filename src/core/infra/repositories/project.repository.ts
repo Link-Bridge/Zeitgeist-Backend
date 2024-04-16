f3c63f6fc6d95d4b625e862383551491fd2604e0
salgue441 <carlossalguero441@gmail.com>
2024-04-16 14:11:24 -0600

    fix(projectFunctionality): saved with prettier for format

--- src/core/infra/repositories/project.repository.ts
+++ src/core/infra/repositories/project.repository.ts
@@ -1,52 +1,47 @@
 import { Prisma } from '../../..';
 import { Project } from '../../domain/entities/project.entity';
-import { mapProjectEntityFromDbModel } from '../mappers/project-entity-from-db-model-mapper'
 import { NotFoundError } from '../../errors/not-found.error';
+import { mapProjectEntityFromDbModel } from '../mappers/project-entity-from-db-model-mapper';
 
 const RESOURCE_NAME = 'Project';
 
 async function findProjectStatusById(id: string) {
-    try {
-        const data = await Prisma.project.findUnique({
-            where: {
-                id:id,
-            },
-            select: {
-                status: true,
-            },
-        });
-    
-        if (!data) {
-            throw new NotFoundError(`${RESOURCE_NAME} status`);
-        }
-    
-        return data; 
+  try {
+    const data = await Prisma.project.findUnique({
+      where: {
+        id: id,
+      },
+      select: {
+        status: true,
+      },
+    });
 
-    } catch (error: unknown) {
-        throw new Error(`${RESOURCE_NAME} repository error`);
+    if (!data) {
+      throw new NotFoundError(`${RESOURCE_NAME} status`);
     }
-    
+
+    return data;
+  } catch (error: unknown) {
+    throw new Error(`${RESOURCE_NAME} repository error`);
+  }
 }
 
-async function findById (id: string) : Promise<Project> {
-    try {
-        let data = await Prisma.project.findUnique({
-            where: {
-                id:id,
-            },
-            
-        });
-        
-        if (!data){
-            throw new NotFoundError(RESOURCE_NAME);
-        }
-        
-        return mapProjectEntityFromDbModel(data);
-        
-    } catch (error: unknown) {
-        throw new Error(`${RESOURCE_NAME} repository error`);
+async function findById(id: string): Promise<Project> {
+  try {
+    let data = await Prisma.project.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!data) {
+      throw new NotFoundError(RESOURCE_NAME);
     }
 
+    return mapProjectEntityFromDbModel(data);
+  } catch (error: unknown) {
+    throw new Error(`${RESOURCE_NAME} repository error`);
+  }
 }
 
 export const ProjectRepository = { findProjectStatusById, findById };
