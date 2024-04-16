ed66b2a328c9e6ff41fcbdf5d6215b2e13502e14
Daniel Hurtado <a01707774@tec.mx>
2024-04-15 21:24:38 -0600

    fix(un-do): restore files

--- src/core/infra/repositories/tasks.repository.ts
+++ src/core/infra/repositories/tasks.repository.ts
@@ -5,22 +5,24 @@ import { mapTaskEntityFromDbModel } from '../mappers/task-entity-from-db-model-m
 
 const RESOURCE_NAME = 'Task';
 
-async function findTasksByProjectId(id_project: string): Promise<Task[]> {
-  try {
-    let data = await Prisma.task.findMany({
-      where: {
-        id_project: id_project,
-      },
-    });
-
-    if (!data) {
-      throw new NotFoundError(RESOURCE_NAME);
+async function findTasksByProjectId (id_project: string) : Promise<Task[]> {
+    try {
+        let data = await Prisma.task.findMany({
+            where: {
+                id_project: id_project
+            },
+        });
+    
+        if (!data) {
+            throw new NotFoundError(RESOURCE_NAME);
+        }
+    
+        return data.map(mapTaskEntityFromDbModel);
+        
+    } catch (error: unknown) {
+        throw new Error(`${RESOURCE_NAME} repository error`);
     }
-
-    return data.map(mapTaskEntityFromDbModel);
-  } catch (error: unknown) {
-    throw new Error(`${RESOURCE_NAME} repository error`);
-  }
+    
 }
 
 export const TaskRepository = { findTasksByProjectId };
