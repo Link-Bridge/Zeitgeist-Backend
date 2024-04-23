b896eb6981126fd3ee77216d3dc641cd4f048b86
Daniel Hurtado <a01707774@tec.mx>
2024-04-22 22:06:00 -0600

    feat(RBAC): add middleware to routes

--- src/api/routes/project.routes.ts
+++ src/api/routes/project.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { SupportedRoles } from '../../utils/enums';
 import { ProjectController } from '../controllers/project.controller';
+import { checkAuthToken } from '../middlewares/auth.middleware';
+import { checkAuthRole } from '../middlewares/rbac.middleware';
 
 const router = Router();
 
-router.get('/report/:id', ProjectController.getReportData);
-router.get('/:clientId', ProjectController.getProjectsClient);
+router.get(
+  '/report/:id',
+  checkAuthToken,
+  checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
+  ProjectController.getReportData
+);
+router.get(
+  '/:clientId',
+  checkAuthToken,
+  checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
+  ProjectController.getProjectsClient
+);
 
 export { router as ProjectRouter };
