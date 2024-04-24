1d52cdb77c5ebff73f0a4aa16e87ce621470efb6
Daniel Hurtado <a01707774@tec.mx>
2024-04-23 22:22:44 -0600

    docs(middleware): add docs

--- src/api/middlewares/rbac.middleware.ts
+++ src/api/middlewares/rbac.middleware.ts
@@ -1,17 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
-import { SupportedRoles } from '../../utils/enums';
 import { EmployeeService } from '../../core/app/services/employee.services';
+import { SupportedRoles } from '../../utils/enums';
 
-async function checkRole(req: Request, res: Response, next: NextFunction, allowedRoles: SupportedRoles[]) {
+/**
+ * Checks in the DB the role of the user
+ * @param req
+ * @param res
+ * @param allowedRoles
+ * @returns
+ */
+async function checkRole(req: Request, res: Response, allowedRoles: SupportedRoles[]) {
   const role = await EmployeeService.findRoleByEmail(req.body.auth.email);
   if (!allowedRoles.includes(role)) {
     return res.status(403).json({ message: 'User is not authorized' });
   }
 }
 
+/**
+ * Middleware to check if the user has the role to access the route
+ * @param allowedRoles
+ * @returns Middleware
+ */
 export const checkAuthRole = (allowedRoles: SupportedRoles[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    await checkRole(req, res, next, allowedRoles);
+    await checkRole(req, res, allowedRoles);
     next();
   };
-}
+};
