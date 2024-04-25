8e457c7951f04d25c7240bc4480c08e282415349
Daniel Hurtado <a01707774@tec.mx>
2024-04-25 16:22:24 -0600

    feat(routes): add routes

--- src/api/routes/company.routes.ts
+++ src/api/routes/company.routes.ts
@@ -6,34 +6,17 @@ import { checkAuthRole } from '../middlewares/rbac.middleware';
 
 const router = Router();
 
-/**
- * @opeanapi
- * /company/:
- *  get:
- *    summary: Obtiene todas las compañías
- *    tags:
- *      - Company
- *    description: Retorna una lista de todas la compañías
- *    responses:
- *      200:
- *        description: Una lista de todas la compañías
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                type: object
- *                properties:
- *                  id:
- *                    type: string
- *                    description: El ID de la compañía
- *                    example: "f4105be8-3b4a-44bb-8707-d1e3eec927ba"
- */
 router.get(
   '/',
   checkAuthToken,
   checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
   CompanyController.getAll
+);
+router.put(
+  '/:id',
+  // checkAuthRole,
+  // checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL]),
+  CompanyController.updateClient
 );
 
 export { router as CompanyRouter };
