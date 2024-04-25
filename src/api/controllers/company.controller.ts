a04a5470f11409af39c40baa75c686f6659f8e85
Daniel Hurtado <a01707774@tec.mx>
2024-04-25 16:22:44 -0600

    feat(company): controller and validate data

--- src/api/controllers/company.controller.ts
+++ src/api/controllers/company.controller.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
 import { CompanyService } from '../../core/app/services/company.service';
+import {
+  zodValidCreatedAtDate,
+  zodValidEmail,
+  zodValidPhoneNumber,
+  zodValidString,
+  zodValidUpdatedAtDate,
+  zodValidUuid,
+} from '../validators/zod.validators';
 
 /**
  * @param {Request} req
@@ -10,7 +19,7 @@ import { CompanyService } from '../../core/app/services/company.service';
  * @throws {Error}
  */
 
-async function getAll(req: Request, res: Response) {
+async function getAll(res: Response) {
   try {
     const data = await CompanyService.findAll();
     res.status(200).json({ data });
@@ -19,4 +28,35 @@ async function getAll(req: Request, res: Response) {
   }
 }
 
-export const CompanyController = { getAll };
+const updateCompanySchema = z.object({
+  company: z.object({
+    id: zodValidUuid,
+    name: zodValidString,
+    email: zodValidEmail,
+    phoneNumber: zodValidPhoneNumber,
+    landlinePhone: zodValidPhoneNumber,
+    archived: z.boolean(),
+    idCompanyDirectContact: zodValidUuid.nullable(),
+    idForm: zodValidUuid.nullable(),
+    createdAt: zodValidCreatedAtDate,
+    updatedAt: zodValidUpdatedAtDate,
+  }),
+});
+
+/**
+ * Receives a request to update a client and validates de data before sending it to the service
+ * @param req
+ * @param res
+ */
+export function updateClient(req: Request, res: Response) {
+  try {
+    const { company } = updateCompanySchema.parse(req.body);
+    const updatedCompany = CompanyService.update(company);
+
+    res.status(200).json({ data: updatedCompany });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export const CompanyController = { getAll, updateClient };
