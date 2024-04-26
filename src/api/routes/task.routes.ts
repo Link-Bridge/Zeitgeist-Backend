8031fcb8ce07ed7883c88a191e845eeaf3ee30d2
ArmandoRosasB <armando.rosas133@gmail.com>
2024-04-25 23:56:19 -0600

    fix(routes): auth

--- src/api/routes/task.routes.ts
+++ src/api/routes/task.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { TaskController } from '../controllers/task.controller';
+import { SupportedRoles } from '../../utils/enums';
+import { checkAuthToken } from '../middlewares/auth.middleware';
+import { checkAuthRole } from '../middlewares/rbac.middleware';
 
 const router = Router();
 
@@ -140,8 +143,8 @@ const router = Router();
 router.post('/create', TaskController.createTask);
 router.get(
   '/:id',
-  // checkAuthToken,
-  //checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
+  checkAuthToken,
+  checkAuthRole([SupportedRoles.CONTABLE, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
   TaskController.getTaskById
 );
 
