2737f717e46dea868970aacf3917f9525a18e99b
sgarnica1 <sgarnica1902@gmail.com>
2024-04-27 17:07:08 -0600

    fix(rbac): fix matching roles

--- src/core/infra/mappers/role-entity-from-db-model.mapper.ts
+++ src/core/infra/mappers/role-entity-from-db-model.mapper.ts
@@ -3,19 +3,19 @@ import { SupportedRoles } from '../../../utils/enums';
 import { RoleEntity } from '../../domain/entities/role.entity';
 
 function supportedRoleFromString(maybeRole: string): SupportedRoles {
-  if (maybeRole.toUpperCase() === SupportedRoles.WITHOUT_ROLE) {
+  if (maybeRole.toUpperCase() === SupportedRoles.WITHOUT_ROLE.toUpperCase()) {
     return SupportedRoles.WITHOUT_ROLE;
   }
 
-  if (maybeRole.toUpperCase() === SupportedRoles.ADMIN) {
+  if (maybeRole.toUpperCase() === SupportedRoles.ADMIN.toUpperCase()) {
     return SupportedRoles.ADMIN;
   }
 
-  if (maybeRole.toUpperCase() === SupportedRoles.LEGAL) {
+  if (maybeRole.toUpperCase() === SupportedRoles.LEGAL.toUpperCase()) {
     return SupportedRoles.LEGAL;
   }
 
-  if (maybeRole.toUpperCase() === SupportedRoles.ACCOUNTING) {
+  if (maybeRole.toUpperCase() === SupportedRoles.ACCOUNTING.toUpperCase()) {
     return SupportedRoles.ACCOUNTING;
   }
 
