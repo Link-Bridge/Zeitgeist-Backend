6f5e619cbecd09237a5bd0f6bb099092874761ef
Daniel Hurtado <a01707774@tec.mx>
2024-04-28 11:59:50 -0600

    fix(mapper): fix mapper

--- src/core/infra/mappers/department-entity-from-db-model.mapper.ts
+++ src/core/infra/mappers/department-entity-from-db-model.mapper.ts
@@ -2,26 +2,10 @@ import { department } from '@prisma/client';
 import { SupportedDepartments } from '../../../utils/enums';
 import { DepartmentEntity } from '../../domain/entities/department.entity';
 
-function supportedDepartmentsFromString(maybeSupportedDepartment: string): SupportedDepartments {
-  if (maybeSupportedDepartment.toUpperCase() === SupportedDepartments.WITHOUT_DEPARTMENT) {
-    return SupportedDepartments.WITHOUT_DEPARTMENT;
-  }
-
-  if (maybeSupportedDepartment.toUpperCase() === SupportedDepartments.ACCOUNTING) {
-    return SupportedDepartments.ACCOUNTING;
-  }
-
-  if (maybeSupportedDepartment.toUpperCase() === SupportedDepartments.LEGAL) {
-    return SupportedDepartments.LEGAL;
-  }
-
-  throw new Error(`Unsupported department: ${maybeSupportedDepartment}`);
-}
-
 export function mapDepartmentEntityFromDbModelToDbModel(department: department): DepartmentEntity {
   return {
     id: department.id,
-    title: supportedDepartmentsFromString(department.title),
+    title: department.title as SupportedDepartments,
     createdAt: department.created_at,
     updatedAt: department.updated_at,
   };
