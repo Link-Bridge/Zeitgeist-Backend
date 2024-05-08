ef47b7a38a991ba811f136aa13f5fc21dd8eb02e
RamonaNF <ramona.najera.fuentes@gmail.com>
2024-05-07 19:40:37 -0600

    fix(backend): add missing auth middlewares

--- src/api/routes/home.routes.ts
+++ src/api/routes/home.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+import { SupportedRoles } from '../../utils/enums';
 import { HomeController } from '../controllers/home.controller';
+import { checkAuthRole } from '../middlewares/rbac.middleware';
 
 const router = Router();
 
 router.get(
   '/:idEmployee',
-  //checkAuthRole([SupportedRoles.ADMIN, SupportedRoles.LEGAL, SupportedRoles.ACCOUNTING]),
+  checkAuthRole([SupportedRoles.ADMIN, SupportedRoles.LEGAL, SupportedRoles.ACCOUNTING]),
   HomeController.getHome
 );
 
