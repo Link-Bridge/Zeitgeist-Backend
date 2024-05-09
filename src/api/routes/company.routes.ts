9064e6c264deb9c24fe881ee805b37993525e93f
Ian326 <a01708940@tec.mx>
2024-05-08 21:14:18 -0600

    refactor(routes): Updated routes orderings

--- src/api/routes/company.routes.ts
+++ src/api/routes/company.routes.ts
@@ -7,28 +7,25 @@ import { checkAuthRole } from '../middlewares/rbac.middleware';
 const router = Router();
 
 router.get(
-  '/:id',
-  checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
-  CompanyController.getUnique
-);
-
-router.get(
   '/',
   checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
   CompanyController.getAll
 );
-
+router.get(
+  '/:id',
+  checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
+  CompanyController.getUnique
+);
+router.post(
+  '/new',
+  checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
+  CompanyController.create
+);
 router.put(
   '/:id',
   checkAuthToken,
   checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
   CompanyController.updateClient
-);
-
-router.post(
-  '/new',
-  checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]),
-  CompanyController.create
 );
 
 export { router as CompanyRouter };
