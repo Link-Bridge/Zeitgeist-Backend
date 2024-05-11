330c52d00fb7774df77d07eaf77ac8f7a3ac689c
salgue441 <carlossalguero441@gmail.com>
2024-05-11 14:52:44 -0600

    fix(createTask): new endpoint for create task

--- src/api/routes/employee.routes.ts
+++ src/api/routes/employee.routes.ts
@@ -6,6 +6,11 @@ import { checkAuthRole } from '../middlewares/rbac.middleware';
 const router = Router();
 
 router.post('/signup', EmployeeController.userExists);
+router.get(
+  '/getEmployees',
+  checkAuthRole([SupportedRoles.ADMIN, SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL]),
+  EmployeeController.getAllEmployees
+);
 
 router.use(checkAuthRole([SupportedRoles.ADMIN]));
 router.get('/', EmployeeController.getAllEmployees);
