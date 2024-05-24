9728271888d397ce6e8fb1db1c3f207652f88171
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2024-05-23 19:21:55 -0600

    fix(clientUpdate): correctly show email error

--- src/api/controllers/company.controller.ts
+++ src/api/controllers/company.controller.ts
@@ -50,7 +50,7 @@ async function updateClient(req: Request, res: Response) {
 
     res.status(200).json(updatedCompany);
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
 
