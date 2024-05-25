61e7b547af95de3b6387a35284ec9ad72c36159d
Ian326 <a01708940@tec.mx>
2024-05-24 18:20:23 -0600

    feat(RF32): Added Endpoint & Logic for 'Expense report by id'

---
+++ src/core/infra/mappers/expense-entity-from-db-model.mapper.ts
@@ -0,0 +1,35 @@
+import { expense } from '@prisma/client';
+import { ExpenseEntity, ExpenseReport, RawExpenseReport } from '../../domain/entities/expense.entity';
+
+export function mapExpenseEntityFromDbModel(model: expense): ExpenseEntity {
+  return {
+    id: model.id,
+    title: model.title,
+    justification: model.justification,
+    supplier: model.supplier ? model.supplier : '',
+    totalAmount: model.total_amount,
+    status: model.status ? model.status : '',
+    category: model.category ? model.category : '',
+    date: model.date,
+    createdAt: model.created_at,
+    updatedAt: model.updated_at ? model.updated_at : undefined,
+    idReport: model.id_report,
+    urlFile: model.url_file ? model.url_file : '',
+  };
+}
+
+export function mapExpenseReportEntityFromDbModel(model: RawExpenseReport): ExpenseReport {
+  return {
+    id: model.id,
+    title: model.title,
+    description: model.description,
+    startDate: model.start_date,
+    endDate: model.end_date ? model.end_date : undefined,
+    status: model.status ? model.status : undefined,
+    urlVoucher: model.url_voucher ? model.url_voucher : '',
+    idEmployee: model.id_employee,
+    employeeFirstName: model.employee?.first_name ? model.employee.first_name : '',
+    employeeLastName: model.employee?.last_name ? model.employee.last_name : '',
+    expenses: model.expense ? model.expense.map(mapExpenseEntityFromDbModel) : [],
+  };
+}
