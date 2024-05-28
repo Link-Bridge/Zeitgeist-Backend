fb4a3e604f8c3571262b63f14ee37776faf7b816
Daniel Hurtado <a01707774@tec.mx>
2024-05-27 22:25:01 -0600

    feat(notif): Add notification endpoint for sending department notifications

--- src/api/routes/notification.routes.ts
+++ src/api/routes/notification.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { SupportedRoles } from '../../utils/enums';
+import { NotificationController } from '../controllers/notification.controller';
+import { checkAuthRole } from '../middlewares/rbac.middleware';
 
 const router = Router();
+
+router.use(checkAuthRole([SupportedRoles.ACCOUNTING, SupportedRoles.LEGAL, SupportedRoles.ADMIN]));
+
+router.post('/send/deparment', NotificationController.sendNotificationToDepartment);
 
 export { router as NotificationRouter };
