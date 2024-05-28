19a235c97f3697584fe2b3fa624d5dc561429160
Daniel Hurtado <a01707774@tec.mx>
2024-05-27 22:25:21 -0600

    chore(employee): Add getAllEmployeesByDepartment function

--- src/core/infra/repositories/employee.repository.ts
+++ src/core/infra/repositories/employee.repository.ts
@@ -95,6 +95,24 @@ async function findByEmail(email: string): Promise<EmployeeEntity | null> {
   }
 }
 
+async function findByDepartment(departmentId: string): Promise<EmployeeEntity[]> {
+  try {
+    const data = await Prisma.employee.findMany({
+      where: {
+        id_department: departmentId,
+      },
+    });
+
+    if (!data) {
+      throw new NotFoundError(RESOURCE_NAME);
+    }
+
+    return data.map(mapEmployeeEntityFromDbModel);
+  } catch (error: unknown) {
+    throw new Error(`Failed to fetch employees by department: ${error}`);
+  }
+}
+
 async function existByEmail(email: string): Promise<boolean> {
   try {
     const data = await Prisma.employee.findUnique({
@@ -136,6 +154,7 @@ export const EmployeeRepository = {
   findAll,
   findByEmail,
   findById,
+  findByDepartment,
   existByEmail,
   updateRoleById,
   deleteEmployeeById,
